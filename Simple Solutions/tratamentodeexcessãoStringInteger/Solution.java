caaa588f81652b9b7460f8f755b4743bad105326
Gustavo Fernandes <gustavo.freitas.afel@gmail.com>
2025-05-02 15:27:38 -0300

    Refatorando com o copilot

--- Simple Solutions/tratamentodeexcessãoStringInteger/Solution.java
+++ Simple Solutions/tratamentodeexcessãoStringInteger/Solution.java
@@ -1,18 +1,24 @@
-import java.io.*;
+// Problem: Bad String
+// Contest: HackerRank - Java Exception Handling (Try-catch)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 
-
-public class Solution {
+class Solution {
+    private static final Logger logger = Logger.getLogger(Solution.class.getName());
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String S = bufferedReader.readLine();
-        try {
-            int n = Integer.parseInt(S);
-            System.out.println(n);
-        } catch (NumberFormatException e) {
-            System.err.println("Bad String");
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+        String inputString = bufferedReader.readLine();
+            try {
+                int n = Integer.parseInt(inputString);
+                if (logger.isLoggable(java.util.logging.Level.INFO)) {
+                    logger.info(String.valueOf(n));
+                }
+            } catch (NumberFormatException e) {
+                logger.warning("Bad String");
+            }
         }
-        bufferedReader.close();
     }
 }
